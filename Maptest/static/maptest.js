a595ac9b411f506850a6174b87f267d9ff37c515
Groshie <36419553+Groshie@users.noreply.github.com>
2022-10-04 19:29:38 +0200

    Update maptest.js

--- Maptest/static/maptest.js
+++ Maptest/static/maptest.js
@@ -1,37 +1,25 @@
-/*
-let test = `entry,keys,pic
-"Degspill kan med fördel användas som näring till mjölmaskar istället för att slängas i hushållssopor.",deg;spill;mjölmask,https://www.mjolmask.se/wp-content/uploads/2019/01/Mj%C3%B6lmask.jpg
-"Ägg kan kokas skalas och avnjutas med en redig tub kaviar!",ägg;kaviar;skal,https://assets.icanet.se/imagevaultfiles/id_33024/cf_259/koka_agg.jpg
-"Ägg kan stekas med gulan intakt vilket kallas sunny side up - för att det känns som att äta solen!",ägg;steka;sol,https://d3a6ymj7hfz0cs.cloudfront.net/wp-content/uploads/2016/03/Agg_stekt.jpg`
-*/
-
-
-
 let db = fetch("data/maptest.csv")
     .then(res => res.text())
     .then(data => db = parseCSV(data));
 
-db
-    .then(data => {
-        let keys = [];
-        let currentKeys = [];
+let keys = [];
+let currentKeys = [];
 
-        for (row in data) {
-            currentKeys = data[row].keys.toString().split(",");
+for (row in db) {
+    currentKeys = db[row].keys.toString().split(",");
 
-            for (key of currentKeys) {
-                if (!keys.includes(key.toString().toLowerCase())) {
-                    keys.push(key.toString().toLowerCase());
-                }
-            }
+    for (key of currentKeys) {
+        if (!keys.includes(key.toString().toLowerCase())) {
+            keys.push(key.toString().toLowerCase());
         }
+    }
+}
 
-        for (key of keys) {
-            let thisOption = document.createElement("option");
-            thisOption.value = key.toString();
-            document.getElementById("entryOptions").append(thisOption);
-        }
-    })
+for (key of keys) {
+    let thisOption = document.createElement("option");
+    thisOption.value = key.toString();
+    document.getElementById("entryOptions").append(thisOption);
+}
 
 function parseCSV(string) {
     let csv = string.split("\r\n");
@@ -51,32 +39,26 @@ function showAnswers() {
     let query = document.getElementById("search-control").value.toString();
     let matches = {};
 
-    db.then(data => {
-        for (row in data) {
-            for (key of data[row].keys) {
-                if (key.toString() == query) {
-                    matches[row] = data[row].entry;
-                }
+    for (row in db) {
+        for (key of db[row].keys) {
+            if (key.toString() == query) {
+                matches[row] = data[row].entry;
             }
         }
+    }
 
-        if (document.getElementById("result-table").childElementCount > 0) {
-            document.getElementById("result-table").innerHTML = "";
-        }
+    if (document.getElementById("result-table").childElementCount > 0) {
+        document.getElementById("result-table").innerHTML = "";
+    }
 
-        for (match in matches) {
-            let thisRow = document.createElement("tr");
-            let thisCell = document.createElement("td");
-            
-            thisCell.append(matches[match]);
-            thisRow.append(thisCell);
-            document.getElementById("result-table").append(thisRow);
-        }
-    })
-
-    
+    for (match in matches) {
+        let thisRow = document.createElement("tr");
+        let thisCell = document.createElement("td");
+        
+        thisCell.append(matches[match]);
+        thisRow.append(thisCell);
+        document.getElementById("result-table").append(thisRow);
+    }
 }
 
 document.getElementById("search-control").addEventListener("change", showAnswers);
-
-showAnswers();
